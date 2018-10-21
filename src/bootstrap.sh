820f470ec81852330ca6016643341e5e039dc9a9
mikamo3 <USER_INFO@users.noreply.github.com>
2018-10-22 00:29:41 +0900

    fix permission

--- src/bootstrap.sh
+++ src/bootstrap.sh
@@ -6,67 +6,69 @@ declare -r DOTFILES_TARBALL_URL="https://github.com/$GITHUB_REPOS/tarball/master
 declare -r DOTFILES_DIRECTORY="$HOME/.dotfiles"
 
 download_repos() {
-  printf "download dotfiles from %s\n" "$DOTFILES_TARBALL_URL"
-  local tmpFile="$(mktemp)"
-  download "$DOTFILES_TARBALL_URL" "$tmpFile"
-  if [ -e "$DOTFILES_DIRECTORY" ]; then
-    printf "%s already exists\n" "$DOTFILES_DIRECTORY"
-    return 1
-  fi
-  mkdir -p "$DOTFILES_DIRECTORY"
-  printf "exact to %s\n" "$DOTFILES_DIRECTORY"
-  extract "$tmpFile" "$DOTFILES_DIRECTORY"
+	printf "download dotfiles from %s\n" "$DOTFILES_TARBALL_URL"
+	local tmpFile=""
+  tmpFile=$(mktemp)
+	download "$DOTFILES_TARBALL_URL" "$tmpFile"
+	if [ -e "$DOTFILES_DIRECTORY" ]; then
+		printf "%s already exists\n" "$DOTFILES_DIRECTORY"
+		return 1
+	fi
+	mkdir -p "$DOTFILES_DIRECTORY"
+	printf "exact to %s\n" "$DOTFILES_DIRECTORY"
+	extract "$tmpFile" "$DOTFILES_DIRECTORY"
 }
 
 download() {
-  local url="$1"
-  local dstFile="$2"
-  if command -v "curl" &> /dev/null; then
-    curl -LsSo "$dstFile" "$url" &> /dev/null
-    return $?
-  elif command -v "wget" &> /dev/null; then
-    wget -qO "$dstFile" "$url" &> /dev/null
-    return $?
-  fi
-  return 1
+	local url="$1"
+	local dstFile="$2"
+	if command -v "curl" &>/dev/null; then
+		curl -LsSo "$dstFile" "$url" &>/dev/null
+		return $?
+	elif command -v "wget" &>/dev/null; then
+		wget -qO "$dstFile" "$url" &>/dev/null
+		return $?
+	fi
+	return 1
 }
 
 extract() {
-  local archive="$1"
-  local outputDir="$2"
+	local archive="$1"
+	local outputDir="$2"
 
-  if command -v "tar" &> /dev/null; then
-    tar zxf "$archive" --strip-components 1 -C "$DOTFILES_DIRECTORY"
-    return $?
-  fi
-  return 1
+	if command -v "tar" &>/dev/null; then
+		tar zxf "$archive" --strip-components 1 -C "$outputDir"
+		return $?
+	fi
+	return 1
 }
 
 check_os() {
-  printf "check os\n"
-  local $os=get_os
-  if [ "$os" == "arch" ]; then
-    return 0
-  fi
-  printf "this script is not supported %s\n" "$os"
-  return 1
+	printf "check os\n"
+	local os=get_os
+	if [ "$os" == "arch" ]; then
+		return 0
+	fi
+	printf "this script is not supported %s\n" "$os"
+	return 1
 }
 
 main() {
-  printf "dotfiles\n"
-  cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
+	printf "dotfiles\n"
+	cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
 
-  if [ $(pwd) = ${DOTFILES_DIRECTORY}/src ]; then
-    . "utils.sh" || exit 1
-  else
-    download_repos || exit 1
-    cd ${DOTFILES_DIRECTORY}/src || exit 1
-    . "utils.sh" exit 1
-  fi
+	if [ $(pwd) == "${DOTFILES_DIRECTORY}/src" ]; then
+		. "utils.sh" || exit 1
+	else
+		download_repos || exit 1
+		cd "${DOTFILES_DIRECTORY}/src" || exit 1
+		. "utils.sh" exit 1
 
-  #install package
-  check_os || exit 1
+	fi
 
-  ./os/install.sh
+	#install package
+	check_os || exit 1
+
+	./os/install.sh
 }
 main "$@"
--- src/os/arch/install.sh
+++ src/os/arch/install.sh
@@ -2,28 +2,28 @@
 cd "$(dirname "${BASH_SOURCE[0]}")" && . "../../utils.sh"
 
 install_package() {
-  declare -r EXTRA_ARGUMENTS="$3"
-  declare -r PACKAGE="$2"
-  declare -r PACKAGE_READABLE_NAME="$1"
+	declare -r EXTRA_ARGUMENTS="$3"
+	declare -r PACKAGE="$2"
+	declare -r PACKAGE_READABLE_NAME="$1"
 
-  if ! package_is_installed "$PACKAGE"; then
-    execute "pacman -S --noconfirm $EXTRA_ARGUMENTS $PACKAGE" "$PACKAGE_READABLE_NAME"
-  else
-    print_success "$PACKAGE_READABLE_NAME"
-  fi
+	if ! package_is_installed "$PACKAGE"; then
+		execute "pacman -S --noconfirm $EXTRA_ARGUMENTS $PACKAGE" "$PACKAGE_READABLE_NAME"
+	else
+		print_success "$PACKAGE_READABLE_NAME"
+	fi
 }
 
 package_is_installed() {
-  pacman -Qs "$1" &> /dev/null
+	pacman -Qs "$1" &>/dev/null
 }
 
 update() {
-  execute "sudo pacman -Syu --noconfirm" "pacman (pacman update)"
+	execute "sudo pacman -Syu --noconfirm" "pacman (pacman update)"
 }
 
 print_in_purple "Install...\n\n"
 ask_for_sudo
 update
 for package in $(cat ../../../packages/arch/pacman-package-list); do
-  install_package "$package (pacman install)" "$package"
+	install_package "$package (pacman install)" "$package"
 done
