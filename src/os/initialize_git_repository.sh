75c6feee287b35668bce5034d4ce3a14053d7f83
mikamo3 <kamo3proj@gmail.com>
2018-10-23 01:33:01 +0900

    add check method

--- src/os/initialize_git_repository.sh
+++ src/os/initialize_git_repository.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-cd "$(dirname "${BASH_SOURCE[0]}")" && . "../../utils.sh"
+cd "$(dirname "${BASH_SOURCE[0]}")" && . "../utils.sh"
+check_git() {
+	if ! command -v "git" % >/dev/null; then
+		print_error "git is not installed!"
+		return 1
+	fi
+}
+
+check_git
