eed1baf2db8392ca02c0523d5aa219320d99897f
mikamo3 <kamo3proj@gmail.com>
2018-10-30 01:11:03 +0900

    no confirm when yay install when yay install successfully then delete work directory

--- src/os/arch/after_install/10_install_yay.sh
+++ src/os/arch/after_install/10_install_yay.sh
@@ -3,11 +3,16 @@
 declare -r YAY_REPOSITORY_URL="https://aur.archlinux.org/yay.git"
 
 cd "$(dirname "${BASH_SOURCE[0]}")" && . "../../../utils.sh"
+
 install_yay() {
   local work_dir
   work_dir=$(mktemp -d)
-  git clone "$YAY_REPOSITORY_URL" "$work_dir" && cd "$work_dir" && makepkg -si
+  git clone "$YAY_REPOSITORY_URL" "$work_dir" && cd "$work_dir" &&
+    makepkg -si --noconfirm &&
+    cd "$(dirname "${BASH_SOURCE[0]}")" &&
+    rm -rf "$work_dir"
 }
+
 main() {
   print_title "Install yay"
   if ! command -v yay; then
