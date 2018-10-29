adc35da3213ffc893b83bbcb8662eb88f7385a0b
mikamo3 <kamo3proj@gmail.com>
2018-10-29 21:57:03 +0900

    move to function

--- src/bootstrap.sh
+++ src/bootstrap.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # Build a new environment
-# 1.Download tarball from github repository (If this script is not running locally)
+# 1.Download tarball from github repository and extract (If this script is not running locally)
 # 2.Install necessary items based on the package list
 # 3.Create symbolic links
+# 4.Initialize dotfiles directory as git repository
 
 declare -r GITHUB_REPOSITORY="mikamo3/dotfiles"
 declare -r TARBALL_URL="https://github.com/$GITHUB_REPOSITORY/tarball/master"
 declare -r UTILS_URL="https://raw.githubusercontent.com/$GITHUB_REPOSITORY/master/src/utils.sh"
+
+declare dotfiles_path="$HOME/.dotfiles"
 
 download() {
   local url="$1"
@@ -26,6 +29,7 @@ download() {
 extract() {
   local source_file_path=$1
   local dst_path=$2
+
   if command -v tar &>/dev/null; then
     tar -zxf "$source_file_path" --strip-components 1 -C "$dst_path"
     return $?
@@ -33,10 +37,54 @@ extract() {
   reutrn 1
 }
 
-main() {
-  local download_temp_file
-  local dotfiles_path="$HOME/.dotfiles"
+download_util_file() {
   local download_util_file
+  download_util_file=$(mktemp)
+  printf "%s\n" "Download utils.sh"
+  printf "  %s\n" "url:$UTILS_URL"
+  download "$UTILS_URL" "$download_util_file" &&
+    . "$download_util_file" &&
+    rm -rf "$download_util_file" &&
+    print_success "Download utils.sh complete" &&
+    return 0
+  printf "%s\n" "Download utils.sh failed"
+  return 1
+}
+
+download_dotfiles_tarball() {
+  local download_temp_file
+
+  #download
+  print_title "Download dotfiles repository"
+  print_info "  url:$TARBALL_URL"
+  download_temp_file=$(mktemp)
+  download "$TARBALL_URL" "$download_temp_file" ||
+    (
+      print_error "Download dotfiles repository"
+      return 1
+    )
+  print_success "Download dotfiles repository"
+
+  #extract tarball
+  print_title "Extract dotfiles tarball"
+  print_title "Create Directory:$dotfiles_path"
+  mkdir -p "$dotfiles_path" ||
+    (
+      print_error "Create Directory:$dotfiles_path"
+      return 1
+    )
+  print_success "Create Directory:$dotfiles_path"
+  print_title "Extract tarball"
+  print_info "  from:$download_temp_file"
+  print_info "  to: $dotfiles_path"
+  extract "$download_temp_file" "$dotfiles_path" &&
+    rm -rf "$download_temp_file" &&
+    print_success "Extract dotfiles tarball" &&
+    return 0
+  print_error "Extract dotfiles tarball"
+  return 1
+}
+main() {
   #TODO: print ascii art
 
   cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
@@ -46,24 +94,13 @@ main() {
   #download utils.sh then source it
   #download dotfiles tarball and extract it
   if ! printf "%s" "${BASH_SOURCE[0]}" | grep -q "bootstrap.sh"; then
+
     #download utils.sh
-    download_util_file=$(mktemp)
-    printf "%s\n" "Download utils.sh"
-    printf "  %s\n" "url:$UTILS_URL"
-    download "$UTILS_URL" "$download_util_file" && . "$download_util_file" && rm -rf "$download_util_file"
-    print_success "Download utils.sh complete"
+    download_util_file || exit 1
     check_os || exit 1
 
-    #download
-    print_title "Download dotfiles repository"
-    print_info "  url:$TARBALL_URL"
-    download_temp_file=$(mktemp)
-    download "$TARBALL_URL" "$download_temp_file" || exit 1
-    print_success "Download dotfiles repository complete"
-
-    #extract tarball
-    print_title "Extract dotfiles tarball"
-    ask_for_confirmation "Dotfiles will extracrted to '$dotfiles_path'. Are you sure?"
+    #decide where to install dotfiles
+    ask_for_confirmation "dotfiles will extracrted to '$dotfiles_path'. Are you sure?"
     if ! answer_is_yes; then
       confirm "Where will you extract dotfiles to? (default: $dotfiles_path)"
       [[ "$REPLY" != "" ]] && dotfiles_path=$(eval "echo $REPLY")
@@ -71,29 +108,22 @@ main() {
 
     if [[ -e "$dotfiles_path" ]]; then
       print_error "$dotfiles_path is already exists. Please remove this directory manually"
-      return 1
+      exit 1
     fi
 
-    print_title "Create Directory:$dotfiles_path"
-    mkdir -p "$dotfiles_path"
-    print_success "Create Directory:$dotfiles_path"
-
-    print_title "Extract tarball"
-    print_info "  from:$download_temp_file"
-    print_info "  to: $dotfiles_path"
-    extract "$download_temp_file" "$dotfiles_path"
-    rm -rf "$download_temp_file"
-    print_success "Extract tarball"
+    #download dotfiles tarball and extract
+    download_dotfiles_tarball || exit 1
   else
     . ./utils.sh
-    dotfiles_path="$(readlink -f "$(pwd)/..")"
+    dotfiles_path="$(readlink -f "$(pwd)/../")"
   fi
   print_info "dotfiles directory :$dotfiles_path"
   cd "$dotfiles_path/src/os" || exit 1
 
-  ./install.sh || exit 1
-  ./symlink.sh || exit 1
-  ./initialize_dotfiles_repository.sh "$GITHUB_REPOSITORY" || exit 1
+  ./install.sh
+  ./symlink.sh
+  ./initialize_dotfiles_repository.sh
+
   print_success "Install complete"
 }
 main "$@"
