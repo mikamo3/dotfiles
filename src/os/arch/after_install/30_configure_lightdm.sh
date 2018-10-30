ea38edcac399c4aef824474a148c11ad79917ae9
mikamo3 <kamo3proj@gmail.com>
2018-10-31 01:18:44 +0900

    add lightdm conf

--- src/os/arch/after_install/30_configure_lightdm.sh
+++ src/os/arch/after_install/30_configure_lightdm.sh
@@ -2,6 +2,8 @@
 # configure_lightdm
 cd "$(dirname "${BASH_SOURCE[0]}")" && . "../../../utils.sh"
 main() {
-  :
+  execute "sudo cp -u ../../../../etc/lightdm/lightdm.conf /etc/lightdm/lightdm.conf" "$(readlink -f ../../../../etc/lightdm/lightdm.conf) → /etc/lightdm/lightdm.conf"
+  execute "sudo systemctl enable lightdm"
 }
-main
+
+main "$@"
