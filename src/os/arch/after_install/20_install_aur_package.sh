c9dce4ed111013a783c88fd0ca92a59d2d55bd5c
mikamo3 <kamo3proj@gmail.com>
2018-10-31 00:52:43 +0900

    fix sudo keepalive timing

--- src/os/arch/after_install/20_install_aur_package.sh
+++ src/os/arch/after_install/20_install_aur_package.sh
@@ -14,7 +14,6 @@ install_package() {
 main() {
   print_title "Install AUR packages with yay"
   local package_list
-  sudo_keepalive
   if ! command -v yay &>/dev/null; then
     print_error "yay is not installed"
     return 1
