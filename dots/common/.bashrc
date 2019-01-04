3f268db46a8b9f4a8d545ee22b9e19f7e968f441
mikamo3 <kamo3proj@gmail.com>
2019-01-05 01:30:52 +0900

    cleanup

--- dots/common/.bashrc
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-[[ $- != *i* ]] && return
-
-alias ls='ls --color=auto'
-alias ll='ls -la'
-PS1='[\u@\h \W]\$ '
--- src/os/common/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd "$(dirname "${BASH_SOURCE[0]}")" && . "../../utils.sh"
-main() {
-  print_title "Install (common)"
-  print_info "Nothing to do"
-}
-main "$@"
