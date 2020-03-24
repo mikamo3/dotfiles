73b2cea6da23fd0206cbe426bc8f0b11e6111993
mikamo3 <kamo3proj@gmail.com>
2020-03-25 02:03:18 +0900

    :children_crossing: (polybar) #5 improve show update count

---
+++ .config/i3/scripts/check_updates.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+UPDATE_RESULT_PATH=~/.local/log/arch_update
+while :
+do
+    if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
+        updates_arch=0
+    fi
+
+    if ! updates_aur=$(yay -Qum 2> /dev/null | wc -l); then
+        updates_aur=0
+    fi
+
+    updates=$(("$updates_arch" + "$updates_aur"))
+    echo "$updates" >> $UPDATE_RESULT_PATH
+    sleep 3600
+done
--- .config/polybar/scripts/do_updates.sh
+++ .config/polybar/scripts/do_updates.sh
@@ -4,3 +4,4 @@ LOCALLOG="~/.local/log/arch_update_$(date +%Y%m%d%H%M%S.log)"
 sh -c "
 yay -Syu --noconfirm  &> $LOCALLOG && notify-send --app-name=$APP_NAME \"update success\" || notify-send --urgency=critical --app-name=$APP_NAME \"update failure\"
 " &
+echo "0" >> $UPDATE_RESULT_PATH
