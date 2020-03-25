211a748de46199e9cd5115213a90a3243f594318
mikamo3 <kamo3proj@gmail.com>
2020-03-26 01:16:22 +0900

    :bug: (polybar) fix set 0 after update

--- .config/polybar/scripts/do_updates.sh
+++ .config/polybar/scripts/do_updates.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 APP_NAME="arch update"
+UPDATE_RESULT_PATH="~/.local/log/arch_update"
 LOCALLOG="~/.local/log/arch_update_$(date +%Y%m%d%H%M%S.log)"
 sh -c "
 yay -Syu --noconfirm  &> $LOCALLOG && notify-send --app-name=$APP_NAME \"update success\" || notify-send --urgency=critical --app-name=$APP_NAME \"update failure\"
+echo 0 >> $UPDATE_RESULT_PATH
 " &
-echo "0" >> $UPDATE_RESULT_PATH
