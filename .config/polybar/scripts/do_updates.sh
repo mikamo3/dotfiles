3d1d0c094e1d7bc8d50bb8c88aee8082fce2b37f
mikamo3 <kamo3proj@gmail.com>
2020-04-05 18:16:23 +0900

    :children_crossing: (polybar) improve yay update notification
    Closes #29

--- .config/polybar/scripts/do_updates.sh
+++ .config/polybar/scripts/do_updates.sh
@@ -2,7 +2,18 @@
 APP_NAME="arch update"
 UPDATE_RESULT_PATH="~/.local/log/arch_update"
 LOCALLOG="~/.local/log/arch_update_$(date +%Y%m%d%H%M%S.log)"
+
 sh -c "
-yay -Syu --noconfirm  &> $LOCALLOG && notify-send --app-name=$APP_NAME \"update success\" || notify-send --urgency=critical --app-name=$APP_NAME \"update failure\"
+if [[ $(ps auwx|grep -c "yay -Sy") != 1 ]];then
+  notify-send --app-name=\"$APP_NAME\" \"now updating\"
+  exit 1
+fi
+
+notify-send --app-name=\"$APP_NAME\" \"start update\"
+yay -Syu --noconfirm  &> $LOCALLOG 
+if [[ $? != 0 ]]; then
+  notify-send --urgency=critical --app-name=\"$APP_NAME\" \"update failure\"
+fi
+notify-send --app-name=\"$APP_NAME\" \"update success\"
 echo 0 >> $UPDATE_RESULT_PATH
 " &
