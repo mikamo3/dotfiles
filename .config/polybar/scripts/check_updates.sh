6abd72f36b92107220eaa162971655a03960e628
mikamo3 <kamo3proj@gmail.com>
2021-09-13 01:09:26 +0900

    :sparkles: (all) reconfigure for new machine

--- .config/polybar/scripts/check_updates.sh
+++ .config/polybar/scripts/check_updates.sh
@@ -1,7 +1,22 @@
-#!/usr/bin/env sh
-UPDATE_RESULT_PATH=/tmp/arch_update
-while [ ! -e "$UPDATE_RESULT_PATH" ]; do
-  echo "0"
-  sleep 1
-done
-tail -n1 -f $UPDATE_RESULT_PATH
+#!/bin/sh
+
+if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
+    updates_arch=0
+fi
+
+if ! updates_aur=$(yay -Qum 2> /dev/null | wc -l); then
+# if ! updates_aur=$(paru -Qum 2> /dev/null | wc -l); then
+# if ! updates_aur=$(cower -u 2> /dev/null | wc -l); then
+# if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
+# if ! updates_aur=$(pikaur -Qua 2> /dev/null | wc -l); then
+# if ! updates_aur=$(rua upgrade --printonly 2> /dev/null | wc -l); then
+    updates_aur=0
+fi
+
+updates=$((updates_arch + updates_aur))
+
+if [ "$updates" -gt 0 ]; then
+    echo "# $updates"
+else
+    echo "0"
+fi
