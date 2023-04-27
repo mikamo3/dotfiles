ef7b1c6d1fad030c102c9d5c64c77c2a6eed8120
mikamo3 <kamo3proj@gmail.com>
2023-04-28 01:53:56 +0900

    :sparkles: (wezterm) use wezterm

--- .bashrc
+++ .bashrc
@@ -8,11 +8,4 @@
 alias ls='ls --color=auto'
 PS1='[\u@\h \W]\$ '
 
-if [[ -z "$TMUX" ]] ;then
-    ID="$( tmux ls | grep -vm1 attached | cut -d: -f1 )" # get the id of a deattached session
-    if [[ -z "$ID" ]] ;then # if not available create a new one
-        exec tmux new-session
-    else
-        exec tmux attach-session -t "$ID" # if available attach to it
-    fi
-fi
+exec fish
---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,108 @@
+local wezterm = require 'wezterm'
+local act = wezterm.action
+local config={}
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+wezterm.on('update-right-status', function(window, pane)
+  local name = window:active_key_table()
+  if name then
+    name = 'TABLE: ' .. name
+  end
+  window:set_right_status(name or '')
+end)
+config.leader = { key = '/', mods = 'ALT' }
+config.keys = {
+  {
+    key = 'a',
+    mods = 'LEADER',
+    action = act.ActivateKeyTable {
+      name = 'resize_pane',
+      one_shot = false,
+    },
+  },
+
+	{ key = "=", mods = "ALT", action = "ResetFontSize" },
+	{ key = "+", mods = "CTRL|SHIFT", action = "IncreaseFontSize" },
+	{ key = "-", mods = "CTRL|SHIFT", action = "DecreaseFontSize" },
+  { key = "-", mods = "ALT", action = act({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
+  { key = "\\", mods = "ALT", action = act({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
+}
+config.key_tables = {
+  resize_pane = {
+    { key = 'LeftArrow', action = act.AdjustPaneSize { 'Left', 1 } },
+    { key = 'h', action = act.AdjustPaneSize { 'Left', 1 } },
+
+    { key = 'RightArrow', action = act.AdjustPaneSize { 'Right', 1 } },
+    { key = 'l', action = act.AdjustPaneSize { 'Right', 1 } },
+
+    { key = 'UpArrow', action = act.AdjustPaneSize { 'Up', 1 } },
+    { key = 'k', action = act.AdjustPaneSize { 'Up', 1 } },
+
+    { key = 'DownArrow', action = act.AdjustPaneSize { 'Down', 1 } },
+    { key = 'j', action = act.AdjustPaneSize { 'Down', 1 } },
+
+    { key = 'LeftArrow', mods = "SHIFT", action = act.ActivatePaneDirection 'Left' },
+    { key = 'h',mods = "SHIFT", action = act.ActivatePaneDirection 'Left' },
+
+    { key = 'RightArrow',mods = "SHIFT", action = act.ActivatePaneDirection 'Right' },
+    { key = 'l',mods = "SHIFT", action = act.ActivatePaneDirection 'Right' },
+
+    { key = 'UpArrow',mods = "SHIFT", action = act.ActivatePaneDirection 'Up' },
+    { key = 'k',mods = "SHIFT", action = act.ActivatePaneDirection 'Up' },
+
+    { key = 'DownArrow',mods = "SHIFT", action = act.ActivatePaneDirection 'Down' },
+    { key = 'j',mods = "SHIFT", action = act.ActivatePaneDirection 'Down' },
+
+    { key = 'Escape', action = 'PopKeyTable' },
+    { key = 'Enter', action = 'PopKeyTable' },
+  },
+}
+
+config.font=wezterm.font("monospace")
+config.font_size=14
+config.use_ime=true
+config.color_scheme="Solarized (dark) (terminal.sexy)"
+config.selection_word_boundary = " \t\n{}[]()\"'`,;:│=&!%"
+config.use_fancy_tab_bar=false
+config.adjust_window_size_when_changing_font_size = false
+config.warn_about_missing_glyphs = false
+config.animation_fps = 1
+config.cursor_blink_ease_in = 'Constant'
+config.cursor_blink_ease_out = 'Constant'
+
+wezterm.on("update-right-status", function(window, pane)
+	local cwd = " "..pane:get_current_working_dir():sub(8).." "; -- remove file:// uri prefix
+	local date = wezterm.strftime(" %H:%M:%S  %Y/%m/%d ");
+	local hostname = " "..wezterm.hostname().." ";
+
+	window:set_right_status(
+		wezterm.format({
+			{Foreground={Color="#ffffff"}},
+			{Background={Color="#005f5f"}},
+			{Text=cwd},
+		})..
+		wezterm.format({
+			{Foreground={Color="#00875f"}},
+			{Background={Color="#005f5f"}},
+			{Text=""},
+		})..
+		wezterm.format({
+			{Foreground={Color="#ffffff"}},
+			{Background={Color="#00875f"}},
+			{Text=date},
+		})..
+		wezterm.format({
+			{Foreground={Color="#00af87"}},
+			{Background={Color="#00875f"}},
+			{Text=""},
+		})..
+		wezterm.format({
+			{Foreground={Color="#ffffff"}},
+			{Background={Color="#00af87"}},
+			{Text=hostname},
+		})
+	);
+end);
+return config
