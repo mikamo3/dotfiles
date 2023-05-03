d211184318b51541904f110e01ae01b007d3cbac
mikamo3 <kamo3proj@gmail.com>
2023-05-03 23:01:36 +0900

    :sparkles: (polybar) add right panel

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -9,7 +9,12 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch Polybar, using default config location ~/.config/polybar/config
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload -c ~/.config/polybar/config.ini top &
+    if [ "$m" == "DisplayPort-0" ]; then
+      MONITOR=$m polybar --reload -c ~/.config/polybar/config.ini top &
+    fi
+    if [ "$m" == "HDMI-A-0" ]; then
+      MONITOR=$m polybar --reload -c ~/.config/polybar/config.ini top_right &
+    fi
     MONITOR=$m polybar --reload -c ~/.config/polybar/config.ini bottom &
   done
 else
