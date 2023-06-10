a989a37d8d0041302a90df8c653fbe08da0dde31
mikamo3 <kamo3proj@gmail.com>
2023-06-10 19:52:19 +0900

    :sparkles: (all) update

--- .bashrc
+++ .bashrc
@@ -8,4 +8,3 @@
 alias ls='ls --color=auto'
 PS1='[\u@\h \W]\$ '
 
-exec fish
--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -9,10 +9,10 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch Polybar, using default config location ~/.config/polybar/config
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    if [ "$m" == "DisplayPort-0" ]; then
+    if [ "$m" == "DP-0" ]; then
       MONITOR=$m polybar --reload -c ~/.config/polybar/config.ini top &
     fi
-    if [ "$m" == "HDMI-A-0" ]; then
+    if [ "$m" == "HDMI-0" ]; then
       MONITOR=$m polybar --reload -c ~/.config/polybar/config.ini top_right &
     fi
     MONITOR=$m polybar --reload -c ~/.config/polybar/config.ini bottom &
