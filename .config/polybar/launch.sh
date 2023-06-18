ee24234b28a76412e3250c6c61bbcc29ed23446d
mikamo3 <kamo3proj@gmail.com>
2023-06-18 20:41:47 +0900

    :sparkles: (minipc) add minipc config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,21 +5,22 @@ killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+profile=$(autorandr --current) 
+if [[ "$profile" == "minipc_single_usbc" ]];then
+  master="DisplayPort-0"
+  slave="" 
+fi
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    if [ "$m" == "DP-0" ]; then
-      MONITOR=$m polybar --reload -c ~/.config/polybar/config.ini top &
-    fi
-    if [ "$m" == "HDMI-0" ]; then
-      MONITOR=$m polybar --reload -c ~/.config/polybar/config.ini top_right &
-    fi
-    MONITOR=$m polybar --reload -c ~/.config/polybar/config.ini bottom &
-  done
-else
-  polybar -r -c ~/.config/polybar/config.ini top  &
-  polybar -r -c ~/.config/polybar/config.ini bottom  &
+if [[ "$(hostname)" == "kamo3workmini" ]];then
+  net_interface="wlan0"
+fi
+
+MONITOR=$master polybar --reload -c ~/.config/polybar/config.ini top &
+MONITOR=$master NET_INTERFACE=$net_interface polybar --reload -c ~/.config/polybar/config.ini bottom &
+
+if [[ $slave != "" ]];then
+  MONITOR=$slave polybar --reload -c ~/.config/polybar/config.ini top_right &
+  MONITOR=$slave polybar --reload -c ~/.config/polybar/config.ini bottom &
 fi
 
 echo "Polybar launched..."
