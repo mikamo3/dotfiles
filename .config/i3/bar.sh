3a1b7ae4840eac2ef4c602ba1ae187bbfa724e06
mikamo3 <kamo3proj@gmail.com>
2024-06-09 00:20:59 +0900

    :sparkles: (i3) mod bar

---
+++ .config/i3/bar.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <bar_id>"
+    exit 1
+elif [ "$1" == "top" ]; then
+bumblebee-status \
+               -m hostname date time xrandr bluetooth2  \
+               -p date.format="%Y-%m-%d" \
+               time.format="%T" \
+               interval=1 \
+               -t solarized-powerline
+elif [ "$1" == "bottom" ]; then
+  bumblebee-status \
+               -m uptime traffic cpu memory\
+               -p traffic.exclude=lo,br-,docker,virbr \
+               interval=1 \
+               -t solarized-powerline
+fi
