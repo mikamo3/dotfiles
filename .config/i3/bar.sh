153cccaaa864b9afaddb6718357f47ca983d6028
mikamo3 <kamo3proj@gmail.com>
2024-06-12 23:57:25 +0900

    :art: (bumblebee-status) change module

--- .config/i3/bar.sh
+++ .config/i3/bar.sh
@@ -5,15 +5,16 @@ if [ -z "$1" ]; then
     exit 1
 elif [ "$1" == "top" ]; then
 bumblebee-status \
-               -m hostname xrandr bluetooth2 date time \
+               -m  xrandr bluetooth2 date time uptime dunst \
                -p date.format="%Y-%m-%d" \
                time.format="%T" \
                interval=1 \
                -t solarized-powerline
 elif [ "$1" == "bottom" ]; then
   bumblebee-status \
-               -m uptime traffic cpu memory\
-               -p traffic.exclude=lo,br-,docker,virbr \
+               -m hostname  nic cpu2 memory disk arch-update \
+               -p nic.exclude=lo,br-,docker,virbr \
                interval=1 \
+               cpu2.layout="cpu2.cpuload cpu2.coresload"  \
                -t solarized-powerline
 fi
