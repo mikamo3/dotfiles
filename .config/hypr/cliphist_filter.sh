0bbab98733ce1abb38582430d6daff9ad157cd8f
mikamo3 <kamo3proj@gmail.com>
2026-01-27 14:50:20 +0900

    fix

--- .config/hypr/cliphist_filter.sh
+++ .config/hypr/cliphist_filter.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 active_class=$(hyprctl activewindow -j | jq -r .class 2>/dev/null)
 
-if [ "$active_class" != "1Password" ]; then
-    cliphist store
+# 除外するアプリケーションのリスト
+if [[ "$active_class" =~ ^(1password|KeePassXC|Bitwarden)$ ]]; then
+    exit 0
 fi
+
+cliphist store
